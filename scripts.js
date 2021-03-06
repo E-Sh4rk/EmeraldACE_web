b487a0cdfb3b6afdc88b0b1459b78adce96e925a
Mickael Laurent <esh4rk@gmail.com>
2021-03-06 23:20:29 +0100

    big update

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
 window.addEventListener ("load", () => {
+    let prefix_for_examples = "files/";
+    let list_path = "files/list.json";
+    let empty_path = "files/empty.txt";
+    let exit_codes_path = "files/exit.txt";
     let code = document.getElementById ("main");
     let exit_codes = document.getElementById ("secondary");
     let select = document.getElementById ("select");
-    let lock = false;
+    let lang = document.getElementById ("lang");
 
-    function getFile(url, field, callback) {
+    let lock = false;
+    let examples = null;
+    let last_selected_example = "";
+
+    function getFile(url, success_callback, callback) {
         lock = true;
         let xhr = new XMLHttpRequest();
         xhr.open("GET", url);
@@ -13,7 +21,7 @@ window.addEventListener ("load", () => {
         xhr.addEventListener("readystatechange", () => {
             if (xhr.readyState == 4) {
                 if (xhr.status == 200)  {
-                    field.value = xhr.responseText;
+                    success_callback(xhr.responseText);
                 } else {
                     console.log("Unknown file " + url);
                 }
@@ -24,18 +32,62 @@ window.addEventListener ("load", () => {
         xhr.send();
     };
 
-    select.addEventListener ("change", () => {
+    function fileToField(url, field, callback) {
+        getFile(url, (content) => {field.value = content;}, callback);
+    }
+
+    function updateCode() {
+        if (examples == null) return;
         if (!lock) {
             if (select.value) {
-                getFile("files/" + select.value + ".txt", code, null);
+                let obj = examples[parseInt(select.value)];
+                fileToField(prefix_for_examples + obj[lang.value], code, null);
             }
             else {
-                code.value = "";
+                fileToField(empty_path, code, null);
             }
         }
+    }
+
+    function updateSelectField() {
+        if (examples == null) return;
+
+        let language = lang.value;
+        //last_selected_example = select.value;
+        let newSelectedIndex = 0;
+        
+        let arrOptions = ["<option value='' selected>----------</option>"];
+        let i = 1;
+        examples.forEach((element,index) => {
+            if (language in element) {
+                let val = index.toString();
+                if (val == last_selected_example) newSelectedIndex = i;
+                arrOptions.push("<option value='"+val+"'>"+element["name"]+"</option>");
+                i += 1;
+            }
+        });
+
+        select.innerHTML = arrOptions.join("\n");
+        select.selectedIndex = newSelectedIndex;
+
+        updateCode();
+    }
+
+    function loadExamples(callback) {
+        getFile(list_path, (content) => {
+            examples = JSON.parse(content);
+            updateSelectField();
+        }, callback);
+    }
+
+    select.addEventListener ("change", () => {
+        last_selected_example = select.value;
+        updateCode();
     });
 
-    getFile("files/exit.txt", exit_codes, () => {
-        getFile("files/empty.txt", code, null);
+    lang.addEventListener ("change", updateSelectField);
+
+    fileToField(exit_codes_path, exit_codes, () => {
+        fileToField(empty_path, code, () => {loadExamples(null);});
     });
 });
